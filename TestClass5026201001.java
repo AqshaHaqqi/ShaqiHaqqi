a2ced2800547bc0807818f7c7fe9551175d17051
AqshaHaqqi <81169717+AqshaHaqqi@users.noreply.github.com>
2021-03-30 00:25:59 +0800

    Add files via upload

---
+++ TestClass5026201001.java
@@ -0,0 +1,15 @@
+import com.example.ComputeMethods5026201001;
+
+public class TestClass5026201001 {
+
+    public static void main(String args[]) {
+        ComputeMethods5026201001 fTC = new ComputeMethods5026201001();
+        System.out.println(fTC.fToC(50F));
+
+        ComputeMethods5026201001 Hypo = new ComputeMethods5026201001();
+        System.out.println(Hypo.hypotenuse(5, 9));
+
+        ComputeMethods5026201001 Dice = new ComputeMethods5026201001();
+        System.out.println(Dice.roll());
+    }
+}
