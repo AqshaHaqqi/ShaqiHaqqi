b6a9ee92e8093e546eef50dcdb765ba30d078e97
AqshaHaqqi <81169717+AqshaHaqqi@users.noreply.github.com>
2021-03-31 13:25:19 +0800

    Update ProcessAName5026201001.java

--- ProcessAName5026201001.java
+++ ProcessAName5026201001.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class ProcessAName5026201001 {
       public static void main(String[] args) {
-                  
+            Substring();
+      }            
+      public static void Substring(){
              Scanner in = new Scanner(System.in);
 
-             System.out.print("Enter your first name: ");
-             String first = in.next();
-             System.out.print("Enter your last name: ");
-             String last = in.next();
-
-             String initials = last + ", " + first.substring(0,1) + ". ";
-             System.out.println(initials);
+             System.out.print("Enter your name: ");
+             String name =input.nextLine;
+             String firstName = name.substring(0,1);
+             String lastName = name.substring(name.indexOf(" ") + 1);
+             int nameLength = name.substring(0,name.indexOf(" ")).lenght();
+             System.out.print(nameLength);
+             System.out.printf("\nYour name is: %s, %s.", lastName, firstName);
        }
 
 }
